ab139b64a2240d3a6ffa09fb8251bee819ff33b4
RahulCh-05 <131550820+RahulCh-05@users.noreply.github.com>
2023-10-27 11:15:50 +0530

    Update JS Assignment-A.js

--- JS Assignment-A.js
+++ JS Assignment-A.js
@@ -1,9 +1,9 @@
 var sentence = prompt();
 var words = sentence.split(" ");
 
-    var reversedWords = words.map(function (word) {
-        return word.split('').reverse().join('');
-    });
+var reversedWords = words.map(function (word) {
+    return word.split('').reverse().join('');
+});
 
 var reversedSentence = reversedWords.join(" ");
 console.log(reversedSentence);
